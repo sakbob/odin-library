610965c10334c94888ddfc14b24720f3cc1d1014
Saksham <atomixdes@icloud.com>
2024-06-12 20:41:57 -0400

    Make array to store Book objects and create basic functions for Book.

---
+++ library-script.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+
+function Book(title, author, pages, hasRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.hasRead = hasRead;
+}
+
+function addBook() {
+
+}
