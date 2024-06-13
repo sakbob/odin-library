b94abedbde7f19a4a1fd2375135941f7a35ce48f
Saksham <atomixdes@icloud.com>
2024-06-12 22:34:57 -0400

    Add forEach loop from array to DOM.

--- library-script.js
+++ library-script.js
@@ -7,6 +7,57 @@ function Book(title, author, pages, hasRead) {
     this.hasRead = hasRead;
 }
 
-function addBook() {
-
+function addBook(book) {
+    myLibrary.concat(book);
 }
+
+const bookContainer = document.querySelector(".book-container");
+const addButton = document.querySelector(".button-add");
+const delButton = document.querySelector(".button-remove");
+const dialog = document.querySelector("#book-dialog");
+const cancelDialog = document.querySelector(".button-cancel");
+const form = document.querySelector("form");
+
+myLibrary.forEach(book => {
+    const htmlBook = document.createElement("div");
+    htmlBook.classList.add("book");
+
+    const title = document.createElement("h6");
+    const author = document.createElement("p");
+    const pages = document.createElement("p");
+    const hasRead = document.createElement("button");
+
+    title.classList.add("book-title");
+    author.classList.add("book-author");
+    pages.classList.add("book-pages");
+    hasRead.classList.add("button-read");
+
+    title.textContent = book.title;
+    author.textContent = book.author;
+    pages.textContent = book.pages;
+
+    if (hasRead === true) {
+        hasRead.style.backgroundColor = "green";
+    }
+
+});
+
+addButton.addEventListener("click", () => {
+    dialog.show();
+});
+
+form.addEventListener("submit", () => {
+    event.preventDefault();
+
+    let author = document.getElementById("author");
+    let title = document.getElementById("title");
+    let pages = document.getElementById("pages");
+    let hasRead = document.getElementById("has-read");
+    
+    dialog.close();
+});
+
+cancelDialog.addEventListener("click", () => {
+    form.reset();
+    dialog.close();
+});
