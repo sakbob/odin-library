bd83170c74e6f498548ac7e5d3447f3f0d5a937b
Saksham <atomixdes@icloud.com>
2024-06-18 19:15:45 -0400

    Refactor code to use class instead of functions.

--- library-script.js
+++ library-script.js
@@ -1,10 +1,12 @@
 const myLibrary = [];
 
-function Book(title, author, pages, hasRead) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.hasRead = hasRead;
+class Book {
+    constructor(title, author,  pages, hasRead) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.hasRead = hasRead;
+    }
 }
 
 function addBook(book) {
